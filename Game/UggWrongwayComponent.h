817a589bf15857d63f9f185511c8de8a96b3c6ec
Haboq <patrik.pecha@student.howest.be>
2026-06-12 06:05:32 +0200

    Quick emscripten fix #2
    Reordering initialization again.

--- Game/UggWrongwayComponent.h
+++ Game/UggWrongwayComponent.h
@@ -27,8 +27,8 @@ namespace dae
             : Component(pOwner)
             , m_isLeftSide(isLeftSide)
             , m_spriteRow(isLeftSide ? 1 : 0)
-            , m_hopInterval(hopInterval)
             , m_spriteCol(isLeftSide ? 0 : 2)
+            , m_hopInterval(hopInterval)
             , m_gridRow(startRow)
             // startCol -1 means use default per side
             , m_gridCol(startCol < 0 ? (isLeftSide ? 0 : (PYRAMID_ROWS - 1)) : startCol)
